dd863b7381f929d81d0289cac5b3b3dd9380845b
Angel Ivan <ian110388@gmail.com>
2024-08-05 21:35:44 -0600

    First commit

---
+++ src/app.http2.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import http2 from 'http2';
+
+// const server = http2.createServer((req,res) => {
+    const server = http2.createSecureServer({
+        key: fs.readFileSync(__dirname.replace('src','') + '/keys/server.key'),
+        cert: fs.readFileSync(__dirname.replace('src','') + '/keys/server.crt'),
+    },(req,res) => {
+    console.log(req.url);
+    // res.write('Hola mundo');
+    // res.end();
+
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write('<h1>Hola mundo!</h1>');
+    // res.end();
+
+    // const data = {name: 'John Doe', age: 30, city: 'New York'};
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.end(JSON.stringify(data));
+    if(req.url === '/') {
+        // const htmlFile = fs.readFileSync('./public/index.html','utf-8');
+        const htmlFile = fs.readFileSync(__dirname + '/public/index.html','utf8');
+        res.writeHead(200 ,{'Content-Type': 'text/html'});
+        res.end(htmlFile);
+        return;
+    }
+
+    if(req.url?.endsWith('.js')) {
+        res.writeHead(200 ,{'Content-Type': 'application/javascript'});
+    } else if(req.url?.endsWith('.css')) {
+        res.writeHead(200 ,{'Content-Type': 'text/css'});
+    }
+
+    try {
+        const responseContent = fs.readFileSync(__dirname + `/public${req.url}`,'utf8');
+        res.end(responseContent);
+    } catch (error) {
+        res.writeHead(404 ,{'Content-Type': 'text/html'});
+        res.end();
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log(`Servidor corriendo en el puerto ${3000}`)
+});
